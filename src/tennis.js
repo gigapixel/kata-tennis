d3d4ccdff0018de4a63fc2f5a2adf97a5e4e4f6d
mmeaww <mwz.thn@gmail.com>
2019-03-05 11:11:40 +0700

    kata - 05032019

--- src/tennis.js
+++ src/tennis.js
@@ -1,52 +1,53 @@
-const scoresBoard = [];
-const playerAScores = [];
-const playerBScores = [];
+const scoreBoards = [];
+const playerA = [];
+const playerB = [];
 
-function getScore(playerScores) {
-  if (!playerScores.length) {
-    return 15;
-  } else if (playerScores.length === 1) {
+function playing(players) {
+  for(let i = 0; i < players.length; i++ ) {
+    let player = players[i];
+    let score;
+    if (player === 'A') {
+      score = getScores(playerA);
+      playerA.push(score);
+    } else if(player === 'B') {
+      score = getScores(playerB);
+      playerB.push(score);
+    }
+
+    scoreBoards.push({team: player, score});
+
+    // set 4
+    // if (i === 3) {
+      let currentA = playerA[playerA.length - 1] || 0;
+      let currentB = playerB[playerB.length - 1] || 0;
+      if (playerA.length === 4) {
+        if (playerA.length - playerB.length >= 2) {
+          console.log('Winner A:', currentA);
+          return;
+        } else if (playerB.length - playerA.length >= 2) {
+          console.log('Winner B:', currentB);
+          return;
+        }
+      }
+      console.log(`In Game => A:${currentA || 0} - B: ${currentB || 0}`);
+    // }
+  }
+
+  console.log('—-> scoreBoards', scoreBoards);
+}
+
+function getScores(player) {
+  let length = player.length;
+  switch (length) {
+    case 0:
+      return 15;
+    case 1:
     return 30;
-  } else if (playerScores.length === 2) {
+    case 2:
     return 40;
+    default:
+    return 'A';
   }
 }
 
-function showScores(scores) {
-  for (let i = 0; i < scores.length; i++) {
-    const s = scores[i];
-    if (s === 'A') {
-      const score = getScore(playerAScores);
-      scoresBoard.push({
-        team: s,
-        score
-      });
-      playerAScores.push(s);
-    } else if (s === 'B') {
-      const score = getScore(playerBScores);
-      scoresBoard.push({
-        team: s,
-        score
-      });
-      playerBScores.push(s);
-    }
-
-    const aScores = scoresBoard.filter(sb => sb.team === 'A');
-    const aResult = aScores[aScores.length - 1] ? aScores[aScores.length - 1].score : 0;
-
-    const bScores = scoresBoard.filter(sb => sb.team === 'B');
-    const bResult = bScores[bScores.length - 1] ? bScores[bScores.length - 1].score : 0;
-
-    console.log(`In Game: A ${aResult} - B ${bResult}`);
-
-    if (playerAScores.length === 3 && playerAScores.length > playerBScores.length) {
-
-    }
-  }
-
-  // console.log('Player A scores is', playerAScores);
-  // console.log('Player B scores is', playerBScores);
-  // console.log(scoresBoard);
-}
-
-showScores(['A', 'B', 'A', 'A']);
+playing(['A', 'A', 'B', 'B', 'A', 'B', 'B', 'A']);
