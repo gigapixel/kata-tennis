91415f87ab54894346f7a9f0accc0bd55f65e7f9
kanasin <kanasin@alphaemgroup.com>
2019-03-05 11:35:21 +0700

    kan

--- src/example.js
+++ src/example.js
@@ -1,55 +1,25 @@
-export function mainReturn(){
-  const input = ['A','B','A','B','A','B','A','A'];
-  const playercount = input.filter((item, index) => input.indexOf(item) == index);
-  if( playercount.length > 2){
-    console.log('case player > 2');
-  }else{
-    const playerName1 = playercount[0];
-    const playerName2 = playercount[1];
-    let player1 = 0;
-    let player2 = 0;
-    input.some(element => {
-      if(element === playerName1){
-        player1 += 1;
-      }else{
-        player2 += 1;
-      }
-      if(player1 === 3 && player2 === 3){
-        player1  -= 1;
-        player2  -= 1;
-      }
-      if(player1 >= 4 || player2 >= 4){
-        console.log(`Winner is ${player1 >= 4 ? playerName1 : playerName2}`);
-        return true;
-      }else{
-        console.log(`InGame : ${checkPoint(player1)} - ${checkPoint(player2)}`);
-      }
-    });
-  }
+function mainFunction(arr){
+  const score = ['0','15','30','40','45'];
+  let scorePlayer = [];
+  
+  const player = [...new Set(arr)];
+  if(player.size>2) return "more two player";
+  if(player.size<2) return "no compettiter";
+  console.log(player)
+  scorePlayer[0] = 0;
+  scorePlayer[1] = 0;
+
+  arr.some((key) => {
+    const playerIndex = (key===player[0]) ? 0 : 1;
+    const otherPlayerIndex = (playerIndex===0) ? 1 : 0;
+    scorePlayer[playerIndex]++;
+    if(scorePlayer[playerIndex]===4 && scorePlayer[otherPlayerIndex]<3){
+      console.log('player ', player[playerIndex] , ' win');
+    }else if(scorePlayer[playerIndex]===5 && scorePlayer[otherPlayerIndex]===3){
+      console.log('player ', player[playerIndex] , ' win');
+    }
+  })
+  console.log(score[scorePlayer[0]]+'/'+score[scorePlayer[1]])
 }
 
-function checkPoint(input){
-  let result;
-  switch(input){
-    case 0: result = 0;
-    break;
-    case 1: result = 15;
-    break;
-    case 2: result = 30;
-    break;
-    case 3: result = 40;
-    break;
-    default: result = 'Winner';
-    break;
-  }
-  return result;
-}
-
-
-// input [A,B,A,A,A]
-// output winner is A : Game - 15
-
-// input [A,B,A]
-// InGame : 30 - 15
-
-console.log(mainReturn());
+console.log(mainFunction(['a','b','a','a','a']))
