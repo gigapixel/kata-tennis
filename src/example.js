5c846e0efb39bc460b5f2d62de84ab31afd5b090
phadthas <phadthas@scg.com>
2019-03-05 10:16:52 +0700

    day2

--- src/example.js
+++ src/example.js
@@ -1,59 +1,47 @@
 import * as _ from 'lodash'
 
+let input1 = ['A', 'B', 'B', 'B', 'B', 'B', 'B', 'B'];
+let input2 = ['A', 'B', 'A', 'B', 'B', 'B', 'B'];
+let input3 = ['A', 'B', 'A', 'A', 'B', 'A', 'B'];
 let scoreArray = [0, 15, 30, 40];
-export function alwaysReturn1(inputs) {
-  let player1score = 0;
-  let player2score = 0;
-  let p1name = _.uniqBy(inputs)[0];
-  let p2name = _.uniqBy(inputs)[1];
-  let due1 = 0;
-  let due2 = 0;
-  console.log('p2name', p2name);
-  console.log('p1name', p1name);
-  // let p2name = inputs.fliter(distinct)[1];
-  // console.log(p1name);
-  let result;
-  inputs.forEach(t => {
 
-    if (t === p1name) {
-      // if (player1score < 3) {
-      player1score++;
-      // }
-    } else if (t === p2name) {
-      // if (player2score < 3) {
-      player2score++;
-      // }
-    }
-    if (player1score === 3 && player2score === 3) {
-      if (t === p1name) {
-        due1++;
-      } else if (t === p2name) {
-        due2++;
+function player(input) {
+  let playerA = _.uniq(input)[0];
+  let playerB = _.uniq(input)[1];
+  let scoreA = 0;
+  let scoreB = 0;
+  let due = false;
+  let result = 'in game';
+  for (let p of input) {
+    if (p === playerA) scoreA++;
+    if (p === playerB) scoreB++;
+    if (scoreA === 4 || scoreB === 4) {
+      if ((scoreA - scoreB) >= 2) {
+        result = 'A Win';
+        break;
       }
-      if (due1 === 1 && due2 === 1) {
-        due1 = 0;
-        due2 = 0;
+      if ((scoreB - scoreA) >= 2) {
+        result = 'B Win';
+        break;
       }
-      if (due1 === 2) {
-        result = (p1name + ' is winner:' + 'Game - ' + scoreArray[3])
-      } else if (due2 === 2) {
-        result = (p2name + ' is winner:' + 'Game - ' + scoreArray[3])
+    }
+    if (scoreA === scoreB && scoreA === 3) {
+      due = true;
+    }
+    if (due) {
+      if ((scoreA - scoreB) === 2) {
+        result = 'A Win';
+        break;
       }
-
+      if ((scoreB - scoreA) === 2) {
+        result = 'B Win';
+        break;
+      }
     }
-
-    console.log('player1score', player1score);
-    if (player1score === 4) {
-      result = (p1name + ' is winner:' + 'Game - ' + scoreArray[player2score])
-    } else if (player2score === 4) {
-      result = (p2name + ' is winner:' + 'Game - ' + scoreArray[player1score])
-    }
-
-
-  });
-
+  }
   return result;
 }
 
-
-console.log(alwaysReturn1(['A', 'A', 'A', 'B', 'B', 'B', 'B']));
+console.log(player(input1));
+//console.log(player(input2));
+//console.log(player(input3));
