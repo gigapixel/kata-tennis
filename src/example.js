6a140fad7a4ff68e49214d3db7d42204cf9059fe
phadthas <phadthas@scg.com>
2019-03-06 10:36:09 +0700

    day3

--- src/example.js
+++ src/example.js
@@ -3,45 +3,73 @@ import * as _ from 'lodash'
 let input1 = ['A', 'B', 'B', 'B', 'B', 'B', 'B', 'B'];
 let input2 = ['A', 'B', 'A', 'B', 'B', 'B', 'B'];
 let input3 = ['A', 'B', 'A', 'A', 'B', 'A', 'B'];
+// let input4 = ['A', 'B', 'A', 'B', 'B', 'A', 'B', 'B']; //B win
+// let input4 = ['A', 'B', 'A', 'B', 'B', 'A', 'A', 'A']; //A win
+// let input4 = ['A', 'B', 'A', 'B', 'B', 'A', 'B', 'A']; //duel
+
+
+
 let scoreArray = [0, 15, 30, 40];
 
-function player(input) {
+export function player(input) {
   let playerA = _.uniq(input)[0];
   let playerB = _.uniq(input)[1];
   let scoreA = 0;
   let scoreB = 0;
   let due = false;
   let result = 'in game';
-  for (let p of input) {
-    if (p === playerA) scoreA++;
-    if (p === playerB) scoreB++;
-    if (scoreA === 4 || scoreB === 4) {
-      if ((scoreA - scoreB) >= 2) {
-        result = 'A Win';
+
+  for (let i of input) {
+    if (i === playerA) scoreA++;
+    if (i === playerB) scoreB++;
+
+    if (due) {
+      if (scoreA - scoreB === 2) {
+        result = score(due, scoreA, scoreB);
         break;
       }
-      if ((scoreB - scoreA) >= 2) {
-        result = 'B Win';
+      if (scoreB - scoreA === 2) {
+        result = score(due, scoreA, scoreB);
         break;
+      }
+    } else {
+      if (scoreA === 4) {
+        if (scoreA >= 2) {
+          result = score(due, scoreA, scoreB);
+          break;
+        }
+      }
+
+      if (scoreB === 4) {
+        if (scoreB >= 2) {
+          result = score(due, scoreA, scoreB);
+          break;
+        }
       }
     }
-    if (scoreA === scoreB && scoreA === 3) {
+
+    if (scoreA === 3 && scoreB === 3) {
       due = true;
     }
-    if (due) {
-      if ((scoreA - scoreB) === 2) {
-        result = 'A Win';
-        break;
-      }
-      if ((scoreB - scoreA) === 2) {
-        result = 'B Win';
-        break;
-      }
-    }
+  }
+  return score(due, scoreA, scoreB);
+}
+function score(due, scoreA, scoreB) {
+  let result;
+  if (due) {
+    if (scoreA === scoreB) { result = 'in game score A : 40 , score B : 40'; }
+    if ((scoreA - scoreB) === 1) { result = 'in game score A : A , score B : 40'; }
+    if ((scoreA - scoreB) === 2) { result = 'A Win , score B : 40'; }
+    if ((scoreB - scoreA) === 1) { result = 'in game score A : 40 , score B : A'; }
+    if ((scoreB - scoreA) === 2) { result = 'B Win score A : 40'; }
+
+  } else {
+    if (scoreA - scoreB >= 2) result = 'A win : B score ' + scoreArray[scoreB];
+    if (scoreB - scoreA >= 2) result = 'B win : A score ' + scoreArray[scoreA];
   }
   return result;
 }
 
 console.log(player(input1));
-//console.log(player(input2));
-//console.log(player(input3));
+console.log(player(input2));
+console.log(player(input3));
--- test/example.test.js
+++ test/example.test.js
@@ -1,13 +1,29 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
-import { alwaysReturn1 } from '../src/example';
+import { player } from '../src/example';
 
 describe('example.js', () => {
-  describe('alwaysReturn1()', () => {
-    it('should return 1', () => {
-      let result = alwaysReturn1();
-      expect(result).equal(1);
+  describe('player()', () => {
+    it('A win', () => {
+      let input = ['A', 'B', 'A', 'A', 'B', 'A', 'B'];
+      let result = player(input);
+      expect(result).equal('A win : B score 30');
+    });
+    it('B win', () => {
+      let input = ['A', 'B', 'B', 'B', 'B', 'A', 'B'];
+      let result = player(input);
+      expect(result).equal('B win : A score 15');
+    });
+    it('Due ingame', () => {
+      let input = ['A', 'A', 'A', 'B', 'B', 'B', 'B'];
+      let result = player(input);
+      expect(result).equal('in game score A : 40 , score B : A');
+    });
+    it('Due B win', () => {
+      let input = ['A', 'A', 'A', 'B', 'B', 'B', 'B', 'B'];
+      let result = player(input);
+      expect(result).equal('B Win score A : 40');
     });
   });
 });
