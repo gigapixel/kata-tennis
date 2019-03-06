36d6957c4e4cc314cb9a56b8f95e802eea3168ae
Sitthi Thiammekha <sitthi@alphaemgroup.com>
2019-03-06 11:11:08 +0700

    code sitthi

--- src/example.js
+++ src/example.js
@@ -1,35 +1,46 @@
-function mainFunction(arr){
-  const score = ['0','15','30','40','45','45'];
-  let scorePlayer = [];
-  let winnerStatus;
-  
-  const player = [...new Set(arr)];
-  if(player.size>2) return "more two player";
-  if(player.size<2) return "no compettiter";
-  console.log(player)
-  scorePlayer[0] = 0;
-  scorePlayer[1] = 0;
-
-  arr.some((key) => {
-    const playerIndex = (key===player[0]) ? 0 : 1;
-    const otherPlayerIndex = (playerIndex===0) ? 1 : 0;
-    scorePlayer[playerIndex]++;
-    //console.log(score[scorePlayer[0]]+'ต่อ'+score[scorePlayer[1]])
-    if(scorePlayer[playerIndex]===4 && scorePlayer[otherPlayerIndex]<3){
-      console.log('player ', player[playerIndex] , ' win');
-      console.log(score[scorePlayer[0]]+'ต่อ'+score[scorePlayer[1]]);
-      winnerStatus = true;
-      return;
-    }else if(scorePlayer[playerIndex]===5 && scorePlayer[otherPlayerIndex]===3){
-      console.log(score[scorePlayer[0]]+'ต่อ'+score[scorePlayer[1]])
-      console.log('player ', player[playerIndex] , ' win');
-      winnerStatus = true;
-      return;
-    }else if(scorePlayer[playerIndex]===4 && scorePlayer[otherPlayerIndex]===4){
-      scorePlayer[otherPlayerIndex] = scorePlayer[otherPlayerIndex]-1;
-    }
-  })
-  if(!winnerStatus) console.log('ผู้เล่น'+player[0]+' '+score[scorePlayer[0]]+'ต่อ'+'ผู้เล่น'+player[1]+' '+score[scorePlayer[1]]);
+function isWin(playerScore) {
+  if (playerScore[playerIndex] === 4 && playerScore[otherPlayerIndex] <= 3) return true;
+  if (playerScore[playerIndex] === 5 && playerScore[otherPlayerIndex] === 4) return true;
+  return false;
 }
 
-console.log(mainFunction(['a','b','a','b','a','b','a','b']))
+async function main(inputArrays) {
+  const player = [... new Set(input)];
+  console.log('player', player);
+  const playerScore = [];
+  playerScore[0] = 0;
+  playerScore[1] = 0;
+
+  return new Promise((resolve) => {
+    inputArrays.some(input => {
+      const playerIndex = (input === player[0]) ? 0 : 1;
+      const otherPlayerIndex = (playerIndex === 1) ? 0 : 1;
+      playerScore[playerIndex]++;
+
+      if (playerScore[playerIndex] === 4 && playerScore[otherPlayerIndex] < 3) {
+        console.log('A');
+        resolve({ status: `${player[playerIndex]} WIN`, score: playerScore });
+      }
+      if (playerScore[playerIndex] === 5 && playerScore[otherPlayerIndex] === 3) {
+        console.log('B');
+        resolve({ status: `${player[playerIndex]} WIN`, score: playerScore });
+      }
+      if (playerScore[playerIndex] === 4 && playerScore[otherPlayerIndex] === 4) {
+        playerScore[playerIndex]--;
+        playerScore[otherPlayerIndex]--;
+      }
+    });
+    if (playerScore[0] >= 3 && playerScore[1] >= 3) {
+      resolve({ status: 'DUCE', score: playerScore });
+    } else {
+      resolve({ status: 'IN GAME', score: playerScore });
+    }
+  });
+}
+
+const score = ['0', '15', '30', '40', 'A', 'G'];
+const input = ['a', 'b', 'a', 'b', 'a', 'b', 'a', 'b', 'a'];
+main(input).then(result => {
+  console.log('result', result.status);
+  console.log('rescoresult', result.score.map(s => score[s]));
+});
