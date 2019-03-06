8189bb2d5702fa51a413f7d53e69dfbc8592c979
ชยากร เจนพิทักษ์ <bankj123@gmail.com>
2019-03-06 11:12:29 +0700

    Unitest tennis

--- test/example.test.js
+++ test/example.test.js
@@ -1,13 +1,29 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
-import { alwaysReturn1 } from '../src/example';
+import { main } from '../src/example';
 
 describe('example.js', () => {
-  describe('alwaysReturn1()', () => {
-    it('should return 1', () => {
-      let result = alwaysReturn1();
-      expect(result).equal(1);
+  describe('main()', () => {
+    it('A WINEER', async () => {
+      let input = ['A','B','A','B','A','B','A','A'];
+      let result = await main(input);
+      expect(result.status).equal('A WIN');
+    });
+    it('B WINNER', async () => {
+      let input = ['A','B','A','B','B','B','B','A'];
+      let result = await main(input);
+      expect(result.status).equal('B WIN');
+    });
+    it('IN GAME', async () => {
+      let input = ['A','B','A','B'];
+      let result = await  main(input);
+      expect(result.status).equal('');
+    });
+    it('DUCE', async () => {
+      let input = ['A','B','A','B','A','B','B','A'];
+      let result = await main(input);
+      expect(result.status).equal('DUCE');
     });
   });
 });
