efd9d7d90fabf3033ed61bc3de8fcb700d92a109
nuthpunnapath <nuthpunnapath@alphaemgroup.com>
2019-03-07 10:23:16 +0700

    add test case 90%(failed)

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 const point = [0, 15, 30, 40];
-
-const input = ['A', 'A', 'A', 'A', 'B']; // A is winner : Game - 0
-//const input = ['B', 'B', 'B', 'B', 'B']; // B is winner : Game - 0
-//const input = ['A', 'A', 'B', 'A', 'A']; // A is winner : Game - 15
-//const input = ['A', 'B', 'A', 'B' ]; // In game : 30-30
-//const input = ['A', 'A', 'A', 'B', 'B' ]; // In game : 40-30
-//const input = ['A', 'A', 'A', 'B', 'B', 'B' ]; // Deuce : A 40 - B 40
-//const input = ['A', 'A', 'A', 'B', 'B', 'B', 'B']; // Deuce : A 40 - B Adv
-//const input = ['A', 'A', 'A', 'B', 'B', 'B', 'A' ]; // Deuce : A Adv - B 40
-//const input = ['A', 'A', 'A', 'B', 'B', 'B', 'A', 'B', 'B' ]; // Deuce : B Adv - B 40
-//const input = ['A', 'A', 'A', 'B', 'B', 'B', 'B', 'B' ]; // B is winner : Game - 40
-//const input = ['A', 'A', 'A', 'B', 'B', 'B', 'B', 'A' ]; // 40 : 40
-//const input = ['A', 'A', 'A', 'B', 'B', 'B','A','B','A','B','A','B','A','B','A','B','A','B' ]; // Deuce : A 40 - B 40
-
-let pointA = 0, pointB = 0;
+// const input = ['A', 'A', 'A', 'A', 'B', 'B']
+let count = 0;
+let pointA = 0;
+let pointB = 0;
 let deuce = false;
 let isWinner = false;
-let count = 0;
-calculatePoint(input);
 
 export function calculatePoint(input) {
-  
   for (let item of input) {
-
     count++;
 
     if (item === 'A') {
@@ -30,57 +15,43 @@ export function calculatePoint(input) {
     } else if (item === 'B') {
       pointB++;
     }
-
-
     if (pointA === 4 && pointB < 4 && !deuce) {
-      console.log('Game - ' + point[pointB]);
+      //console.log('Game - ' + point[pointB]);
       isWinner = true;
-      return 'Game - ' + point[pointB];
-      break;
+      return 'Winner is A : Game - ' + point[pointB];
+
     } else if (pointB === 4 && pointA < 4 && !deuce) {
       isWinner = true;
-      console.log(point[pointA] + ' - Game');
-      return point[pointA] + ' - Game';
-      break;
-    } else if (pointA === 3 && pointB === 3 && !deuce) {
+      //console.log(point[pointA] + ' - Game');
+      return + 'Winner is B : ' + point[pointA] + ' - Game';
+
+    } else if (pointA === 3 && pointB === 3) {
       deuce = true;
     }
-
     if (deuce) {
+
       if (pointA - pointB === 2) {
-        isWinner = true;
-        //console.log('deuce');
-        console.log('Game - 40');
-        break;
+        if (count === input.length) {
+          isWinner = true;
+          return ('Winner is A: Game -' + pointB);
+        }
       } else if (pointB - pointA === 2) {
-        isWinner = true;
-        //console.log('deuce');
-        console.log('40 - Game');
-        break;
+        if (count === input.length) {
+          console('Winner is B:' + pointA + '- Game');
+          return ('Winner is B:' + pointA + '- Game');
+        }
+
       } else {
-        if (pointA > pointB) {
-          //console.log('deuce');
-          if(count === input.length) {
-            console.log('Deuce A - 40');
-          }
-        } else if (pointB > pointA) {
-          if(count === input.length) {
-            console.log('Deuce 40 - A');
-          }
-        } else {
-          if(count === input.length) {
-            console.log('40 - 40');
-          }
-        }    
+        if (count === input.length) {
+          console('In Game 40 - 40');
+          return ('In Game 40 - 40');
+        }
+        if (!isWinner && !deuce) {
+          let scoreA = point[pointA] === undefined ? 0 : point[pointA];
+          let scoreB = point[pointB] === undefined ? 0 : point[pointB];
+          return ('In Game :' + scoreA + ' - ' + scoreB);
+        }
       }
     }
   }
-
-  if (!isWinner && !deuce) {
-    let scoreA = point[pointA] === undefined ? 0 : point[pointA];
-    let scoreB = point[pointB] === undefined ? 0 : point[pointB];
-
-    console.log(`In game ${scoreA} - ${scoreB}`);
-  }
-
 }
--- test/example.test.js
+++ test/example.test.js
@@ -4,19 +4,113 @@ import { expect } from 'chai';
 import { calculatePoint } from '../src/index';
 
 
+
+// Winner is A : Game - [Score B]
+// Winner is B : [Score B] - Game
+// In Game : 40 - A
+// In Game : A - 40
+// In Game : 40 - 40
+
+
+// 1 A ชนะ B = 0
+// 2 A ชนะ B != 0
+// 3 B ชนะ A = 0
+// 4 B ชนะ A != 0
+// 5 ดิว คะแนนเท่ากัน
+// 6 ดิว A นำ
+// 7 ดิว B นำ
+// 8 ดิวสลับไปมาแล้ว A นำ 
+// 9 ดิวสลับไปมาแล้ว B นำ 
+// 10 ดิวสลับไปมาแล้วคะแนนเท่ากัน A นำ B ตาม แล้ว B ได้อีกแต้มตามมาเท่ากัน
+// 11 ดิวสลับไปมาแล้ว A ชนะ
+// 12 ดิวสลับไปมาแล้ว B ชนะ
+
 describe('index.js', () => {
   describe('calculatePoint()', () => {
-    
-    it('should return Game - 0', () => {
-      const input = ['A', 'A', 'A', 'A', 'B']; // A is winner : Game - 0
+
+    // 1
+    it('should return Winner is A : Game - 0 when input is [A, A, A, A, B]', () => {
+      const input = ['A', 'A', 'A', 'A', 'B'];
       let result = calculatePoint(input);
-      expect(result).equal('Game - 0');
+      expect(result).equal('Winner is A : Game - 0');
     });
 
-    // it('should return 2', () => {
-    //   const input = ['B', 'B', 'B', 'B', 'B'];
-    //   let result = calculatePoint(input);
-    //   expect(result).equal('0 - Game');
-    // });
+    // 2
+    it('should return Winner is B : 0 - Game when input is [B, B, B, B, A, B]', () => {
+      const input = ['B', 'B', 'B', 'B', 'A', 'B'];
+      let result = calculatePoint(input);
+      expect(result).equal('Winner is B : 0 - Game');
+    });
+
+    // 3
+    it('should return Winner is A : Game - 15 when input is [A, A, B, A, A]', () => {
+      const input = ['A', 'A', 'B', 'A', 'A'];
+      let result = calculatePoint(input);
+      expect(result).equal('Winner is A : Game - 15');
+    });
+
+    // 4
+    it('should return Winner is B : 15 - Game when input is [B, B, A, B ,B]', () => {
+      const input = ['B', 'B', 'A', 'B', 'B'];
+      let result = calculatePoint(input);
+      expect(result).equal('Winner is B : 15 - Game');
+    });
+
+    // 5
+    it('should return In Game : 40 - 40 when input is [B, B, A, A, B, A]', () => {
+      const input = ['B', 'B', 'A', 'A', 'B', 'A'];
+      let result = calculatePoint(input);
+      expect(result).equal('In Game : 40 - 40');
+    });
+
+    // 6
+    it('should return In Game : A - 40 when input is [B, B, A, A, B, A, A]', () => {
+      const input = ['B', 'B', 'A', 'A', 'B', 'A', 'A'];
+      let result = calculatePoint(input);
+      expect(result).equal('In Game : A - 40');
+    });
+
+    // 7
+    it('should return In Game : 40 - A when input is [B, B, A, A, B, A, B]', () => {
+      const input = ['B', 'B', 'A', 'A', 'B', 'A', 'B'];
+      let result = calculatePoint(input);
+      expect(result).equal('In Game : 40 - A');
+    });
+
+    // 8
+    it('should return In Game : A - 40 when input is [B, B, A, A, A, B, A, B, A]', () => {
+      const input = ['B', 'B', 'A', 'A', 'A', 'B', 'A', 'B', 'A'];
+      let result = calculatePoint(input);
+      expect(result).equal('In Game : A - 40');
+    });
+
+    // 9
+    it('should return In Game : 40 - A when input is [B, B, A, A, A, B, A, B, B, B]', () => {
+      const input = ['B', 'B', 'A', 'A', 'A', 'B', 'A', 'B', 'B', 'B'];
+      let result = calculatePoint(input);
+      expect(result).equal('In Game : 40 - A');
+    });
+
+    // 10
+    it('should return In Game : 40 - 40 when input is [B, B, A, A, A, B, A, B, B]', () => {
+      const input = ['B', 'B', 'A', 'A', 'A', 'B', 'A', 'B', 'B'];
+      let result = calculatePoint(input);
+      expect(result).equal('In Game : 40 - 40');
+    });
+
+
+    // 11
+    it('should return Winner is A : Game - 40 when input is [B, B, A, A, A, B, A, B, A, B, A, B, A]', () => {
+      const input = ['B', 'B', 'A', 'A', 'A', 'B', 'A', 'B', 'A', 'B', 'A', 'B', 'A'];
+      let result = calculatePoint(input);
+      expect(result).equal('Winner is A : Game - 40');
+    });
+
+    // 12
+    it('should return Winner is B : 40 - Game when input is [B, B, A, A, B, A, B, A, B, A, B, B, B]', () => {
+      const input = ['B', 'B', 'A', 'A', 'B', 'A', 'B', 'A', 'B', 'A', 'B', 'B', 'B'];
+      let result = calculatePoint(input);
+      expect(result).equal('Winner is B : 40 - Game');
+    });
   });
 });
