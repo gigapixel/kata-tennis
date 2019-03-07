b271d187eaf63f4a17d275403fb4eba986ab5db7
kookoo <koo_g0kek@hotmail.com>
2019-03-07 10:24:13 +0700

    20190307

--- src/example.js
+++ src/example.js
@@ -1,66 +1,167 @@
-let input = ['A', 'B', 'B', 'B','A','A','A'];
-let scoreA = 0;
-let scoreB = 0;
-let deuce = null;
-let gemescore = [0, 15, 30, 40, 'Adv'];
-let isEnd = false;
 
-input.forEach(s => {
-  if(scoreA >= 3 && scoreB >= 3){
-    if(!deuce){
-      deuce = s;
-      if (s === 'A') {
-        scoreA++;
-      }
-      else if (s === 'B') {
-        scoreB++;
-      }
-    }else{
-      if(deuce === s){
-        console.log(deuce);
-        // winner s
-        // if (s === 'A') {
-        //   scoreA++;
-        // }
-        // else if (s === 'B') {
-        //   scoreB++;
-        // }
-        isEnd = true;
+
+export class Tennis {
+  constructor() {
+
+    this.scoreA = 0;
+    this.scoreB = 0;
+    this.deuce = null;
+    this.gameScore = [0, 15, 30, 40, 'A'];
+    this.winner = null;
+  }
+
+  playGame(input) {
+
+    input.forEach(e => {
+      this.countScore(e);
+      if (this.winner) {
         return;
-      }else{
-        deuce = null;
-          scoreA = 3;
-          scoreB = 3;
-          isEnd = false;
+      }
+    });
+
+    const result = this.showResult();
+    console.log('result >>>>>>>>', result);
+  }
+
+  showResult() {
+    if (this.winner) {
+      if (this.winner === 'A') {
+        return 'Winner is A : Game - ' + this.gameScore[this.scoreB];
+      }
+      if (this.winner === 'B') {
+        return 'Winner is B : Game - ' + this.gameScore[this.scoreA];
       }
     }
-  }else{
-    if (s === 'A') {
-      scoreA++;
-    }
-    else if (s === 'B') {
-      scoreB++;
+    else {
+      return 'Ingame : ' + this.gameScore[this.scoreA] + ' - ' + this.gameScore[this.scoreB];
     }
   }
-});
+  countScore(p) {
 
-if (isEnd){
+    if (this.winner)
+      return;
+
+    if (p === 'A') {
+      this.scoreA++;
+    }
+    if (p === 'B') {
+      this.scoreB++;
+    }
+
+    if (this.scoreA >= 3 && this.scoreB >= 3) {
+
+      if (this.deuce) {
+        if (this.deuce === p) {
+          this.winner = p;
+          this.deuce = null;
+          return;
+        }
+        else {
+          this.deuce = null;
+          this.scoreA = 3;
+          this.scoreB = 3;
+        }
+      }
+      else {
+        this.deuce = p;
+      }
+    }
+    else{
+      if (p === 'A'&&this.scoreA>=4) {
+        this.winner = p;
+      }
+      if (p === 'B'&&this.scoreB>=4) {
+        this.winner = p;
+      }
+    }
+
+
+    // console.log();
+  }
 
 }
-if(scoreA >= 4){
-  console.log('A is winner : Game - ' + gemescore[scoreB]);
-} else if (scoreB >= 4){
-  console.log('B is winner : Game - ' + gemescore[scoreA]);
-}else{
-  console.log('in game A = ' + gemescore[scoreA] + ' - B= ' + gemescore[scoreB]);
-}
-
-// summary
 
 
-export function  alwaysReturn1() {
-  return 1;
-}
+let input = ['A', 'B', 'B', 'B', 'B', 'A', 'A'];
 
-console.log(alwaysReturn1());
+// let input = ['A', 'A', 'A', 'B', 'B', 'B', 'B'];
+
+let tennis = new Tennis();
+tennis.playGame(input);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let isEnd = false;
+
+// input.forEach(s => {
+//   if(scoreA >= 3 && scoreB >= 3){
+//     if(!deuce){
+//       deuce = s;
+//       if (s === 'A') {
+//         scoreA++;
+//       }
+//       else if (s === 'B') {
+//         scoreB++;
+//       }
+//     }else{
+//       if(deuce === s){
+//         console.log(deuce);
+//         // winner s
+//         // if (s === 'A') {
+//         //   scoreA++;
+//         // }
+//         // else if (s === 'B') {
+//         //   scoreB++;
+//         // }
+//         isEnd = true;
+//         return;
+//       }else{
+//         deuce = null;
+//           scoreA = 3;
+//           scoreB = 3;
+//           isEnd = false;
+//       }
+//     }
+//   }else{
+//     if (s === 'A') {
+//       scoreA++;
+//     }
+//     else if (s === 'B') {
+//       scoreB++;
+//     }
+//   }
+// });
+
+// if (isEnd){
+
+// }
+// if(scoreA >= 4){
+//   console.log('A is winner : Game - ' + gameScore[scoreB]);
+// } else if (scoreB >= 4){
+//   console.log('B is winner : Game - ' + gameScore[scoreA]);
+// }else{
+//   console.log('in game A = ' + gameScore[scoreA] + ' - B= ' + gameScore[scoreB]);
+// }
+
+// // summary
+
+
+// export function  alwaysReturn1() {
+//   return 1;
+// }
+
+// console.log(alwaysReturn1());
 
--- test/example.test.js
+++ test/example.test.js
@@ -1,10 +1,10 @@
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
 
-import { alwaysReturn1 } from '../src/example';
+import { Tennis } from '../src/example';
 
 describe('example.js', () => {
-  describe('alwaysReturn1()', () => {
+  describe('playGame()', () => {
     it('should return 1', () => {
       let result = alwaysReturn1();
       expect(result).equal(1);
