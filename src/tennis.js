b032a4a6f98688736c6db743762b7a9d55592c4e
mmeaww <mwz.thn@gmail.com>
2019-03-08 11:07:42 +0700

    updated

--- src/tennis.js
+++ src/tennis.js
@@ -1,31 +1,18 @@
-
 export function playing(players) {
-  let scoreBoadrs = [];
   let playerA = [];
   let playerB = [];
 
   for (let player of players) {
-    let score;
-    if (player === 'A') {
-      score = getScore(playerA);
-      playerA.push(score);
-    }
-    if (player === 'B') {
-      score = getScore(playerB);
-      playerB.push(score);
-    }
+    if (player === 'A') playerA.push(getScore(playerA));
+    if (player === 'B') playerB.push(getScore(playerB));
 
     let currentA = playerA[playerA.length - 1] || 0;
     let currentB = playerB[playerB.length - 1] || 0;
-    if (playerA.length > 3 && (playerA.length - playerB.length) >= 2) {
-      return `Winner is A: ${currentA} - ${currentB}`;
-    } else if (playerB.length > 3 && (playerB.length - playerA.length) >= 2) {
-      return `Winner is B: ${currentA} - ${currentB}`;
-    } else {
-      if (currentA === 'A' && currentB === 'A') {
-        playerA.splice(playerA.length - 1, 1);
-        playerB.splice(playerB.length - 1, 1);
-      }
+    if (playerA.length > 3 && (playerA.length - playerB.length) >= 2) return `Winner is A: Game - ${currentB}`;
+    if (playerB.length > 3 && (playerB.length - playerA.length) >= 2) return `Winner is B: ${currentA} - Game`;
+    if (currentA === 'A' && currentB === 'A') {
+      playerA.splice(playerA.length - 1, 1);
+      playerB.splice(playerB.length - 1, 1);
     }
   }
 
@@ -34,19 +21,14 @@ export function playing(players) {
 
 export function getScore(score) {
   switch (score.length) {
-    case 0:
-      return 15;
-    case 1:
-      return 30;
-    case 2:
-      return 40;
-    case 3:
-      return 'A';
-    default:
-      return 'G';
+    case 0: return 15;
+    case 1: return 30;
+    case 2: return 40;
+    case 3: return 'A';
+    default: return 'G';
   }
 }
 
 
-// const res = playing(['A', 'B', 'A', 'B', 'A', 'B', 'B', 'A', 'A', 'B', 'B', 'B']); 
-// console.log('res', res);
+const res = playing(['A', 'B', 'A', 'B', 'A']); 
+console.log('res', res);
--- test/tennis.test.js
+++ test/tennis.test.js
@@ -6,19 +6,71 @@ import { playing } from '../src/tennis';
 describe('tennis.js', () => {
   describe('playing()', () => {
     it('should return winner is A', () => {
-      let result = playing(['A', 'A', 'B', 'A', 'A']);
-console.log('result', result);
-      // expect(result).equal(expected);
+      let result = playing(['A', 'A', 'A', 'A']);
+      console.log('result', result);
+      expect(result).equal('Winner is A: Game - 0');
+    });
+    it('should return winner is A', () => {
+      let result = playing(['A', 'A', 'A', 'B', 'A']);
+      console.log('result', result);
+      expect(result).equal('Winner is A: Game - 15');
+    });
+    it('should return winner is A', () => {
+      let result = playing(['A', 'A', 'A', 'B', 'B', 'A']);
+      console.log('result', result);
+      expect(result).equal('Winner is A: Game - 30');
+    });
+    it('should return winner is A', () => {
+      let result = playing(['A', 'A', 'B', 'B', 'A', 'B', 'A', 'A']);
+      console.log('result', result);
+      expect(result).equal('Winner is A: Game - 40');
+    });
+
+    it('should return winner is B', () => {
+      let result = playing(['B', 'B', 'B', 'B']);
+      console.log('result', result);
+      expect(result).equal('Winner is B: 0 - Game');
     });
     it('should return winner is B', () => {
-      let result = playing(['A', 'B', 'B', 'A', 'B', 'B']);
-console.log('result', result);
-      // expect(result).equal(expected);
+      let result = playing(['B', 'B', 'B', 'A', 'B']);
+      console.log('result', result);
+      expect(result).equal('Winner is B: 15 - Game');
     });
     it('should return winner is B', () => {
-      let result = playing(['A']);
-console.log('result', result);
-      // expect(result).equal(expected);
+      let result = playing(['B', 'B', 'B', 'A', 'A', 'B']);
+      console.log('result', result);
+      expect(result).equal('Winner is B: 30 - Game');
+    });
+    it('should return winner is B', () => {
+      let result = playing(['B', 'B', 'A', 'A', 'B', 'A', 'B', 'B']);
+      console.log('result', result);
+      expect(result).equal('Winner is B: 40 - Game');
+    });
+
+    it('should return ingame: 0 - 15', () => {
+      let result = playing(['B']);
+      console.log('result', result);
+      expect(result).equal('Ingame: 0 - 15');
+    });
+    it('should return ingame: A - 40', () => {
+      let result = playing(['A', 'B', 'B', 'B', 'A', 'A', 'A']);
+      console.log('result', result);
+      expect(result).equal('Ingame: A - 40');
+    });
+    it('should return ingame: 40 - A', () => {
+      let result = playing(['A', 'B', 'B', 'B', 'A', 'A', 'B']);
+      console.log('result', result);
+      expect(result).equal('Ingame: 40 - A');
+    });
+    it('should return ingame: 40 - 40', () => {
+      let result = playing(['A', 'B', 'B', 'B', 'A', 'A']);
+      console.log('result', result);
+      expect(result).equal('Ingame: 40 - 40');
+    });
+    it('should return ingame: 40 - 40', () => {
+      let result = playing(['A', 'B', 'B', 'B', 'A', 'A', 'B', 'A']);
+      console.log('result', result);
+      expect(result).equal('Ingame: 40 - 40');
     });
   });
 });
